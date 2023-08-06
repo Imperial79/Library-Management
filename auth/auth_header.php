084d5833349e72c48644b646ae4d653f7f94fb55
Imperial@79 <avishekverma79@gmail.com>
2023-08-06 12:03:25 +0530

    init

---
+++ auth/auth_header.php
@@ -0,0 +1,29 @@
+<?php
+
+$scriptFilename = explode('/', $_SERVER['SCRIPT_FILENAME']);
+$currentpath = $scriptFilename[count($scriptFilename) - 1];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Tailwind import -->
+        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.css" rel="stylesheet" />
+
+        <!-- Google Font import "Poppins" -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;800&display=swap"
+            rel="stylesheet">
+
+        <!-- CSS Import -->
+        <link rel="stylesheet" type="text/css" href="style/index.css">
+
+
+        <title><?php echo $currentpath == "index.php" ? "Login" : "Register"; ?> | HITK Library</title>
+    </head>
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php include "./auth/auth_header.php"; ?>
+
+<head></head>
+
+<body style="background-color: whitesmoke; padding: 20px; margin-top: 40px; font-family: 'Poppins', sans-serif;">
+
+
+
+    <div class="column">
+        <div style="margin-left: 40px; margin-bottom: 40px;">
+
+            <div style="flex-direction: row; display: flex;">
+                <img src="assets/hit_logo.png" alt="" width="100">
+
+                <div class="column" style="padding-left: 20px;">
+                    <h1 style="font-size: 40px; font-weight: 700;">
+                        Login
+                    </h1>
+                    <h2>
+                        to Heritage Library
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row flex-container px-20" style="margin-top: 20px;">
+
+
+
+            <div class="flex-item">
+                <img src="./assets/library.png" alt="" height="450" width="450">
+            </div>
+
+
+
+            <div class="flex-item" style=" align-self: center; max-width: 500px;">
+                <form>
+                    <div class="mb-6">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your
+                            email</label>
+                        <input type="email" id="email"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500"
+                            placeholder="name@example.com" required>
+                    </div>
+                    <div class="mb-6">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your
+                            password</label>
+                        <input type="password" id="password"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500"
+                            required>
+                    </div>
+                    <div class="flex items-start mb-6">
+                        <div class="flex items-center h-5">
+                            <input id="remember" type="checkbox" value=""
+                                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 light:bg-gray-700 light:border-gray-600 light:focus:ring-blue-600 light:ring-offset-gray-800 light:focus:ring-offset-gray-800"
+                                required>
+                        </div>
+                        <label for="remember"
+                            class="ml-2 text-sm font-medium text-gray-900 light:text-gray-300">Remember
+                            me</label>
+                    </div>
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 light:bg-blue-600 light:hover:bg-blue-700 light:focus:ring-blue-800">
+                        Proceed
+                    </button>
+
+                    <a type="button" href="./register.php"
+                        class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 light:focus:ring-gray-700 light:bg-gray-800 light:text-gray-400 light:border-gray-600 light:hover:text-white light:hover:bg-gray-700">
+                        Don't have an account? Register
+                    </a>
+                </form>
+            </div>
+
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
